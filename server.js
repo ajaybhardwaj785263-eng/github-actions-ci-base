308bb6a5e897a42c1384a64c1db50460aec7299f
Ajay Bhardwaj <ajaybhardwaj785263-eng@users.noreply.github.com>
2025-11-08 16:54:51 -0700

    Initial commit - CPSY 350 Project GitHub Actions CI

---
+++ server.js
@@ -0,0 +1,29 @@
+'use strict';
+
+
+
+const express = require('express');
+
+const PORT = 8080;
+
+const HOST = '0.0.0.0';
+
+
+
+const app = express();
+
+app.get('/', (req, res) => {
+
+  res.send('CPSY 350 Project: Github Actions CI. SAIT ID:000123456');
+
+});
+
+
+
+app.listen(PORT, HOST, () => {
+
+  console.log(`Running on http://${HOST}:${PORT}`);
+
+});
+
+
