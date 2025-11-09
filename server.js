43c474623449fb675ab81243fcfbd6ad056eb92a
ajaybhardwaj785263-eng <ajaybhardwaj785263@gmail.com>
2025-11-09 16:23:49 -0700

    Export app and server for testing purposes
    Refactor server.js to export app and server for testing.

--- server.js
+++ server.js
@@ -1,29 +1,19 @@
 'use strict';
 
-
-
 const express = require('express');
 
 const PORT = 8080;
-
 const HOST = '0.0.0.0';
-
-
 
 const app = express();
 
 app.get('/', (req, res) => {
-
   res.send('CPSY 350 Project: Github Actions CI. SAIT ID:000123456');
-
 });
 
-
-
-app.listen(PORT, HOST, () => {
-
+const server = app.listen(PORT, HOST, () => {
   console.log(`Running on http://${HOST}:${PORT}`);
-
 });
 
-
+// ✅ Export both app and server so tests can use and close them
+module.exports = { app, server };
