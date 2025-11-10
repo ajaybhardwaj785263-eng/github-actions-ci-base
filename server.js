d6ea0a70ba44f79aa8ee8835338baf4c91785007
ajaybhardwaj785263-eng <ajaybhardwaj785263@gmail.com>
2025-11-10 04:56:10 -0700

    Fix server.js to properly start Express app
    Updated server port from 3000 to 8080 and modified server start logic.

--- server.js
+++ server.js
@@ -3,16 +3,17 @@
 const express = require('express');
 
 // Constants
-const PORT = 3000;
+const PORT = 8080;
 const HOST = '0.0.0.0';
 
 // App
 const app = express();
+
 app.get('/', (req, res) => {
-  res.send('CPSY 350 Project: Github Actions CI. SAIT ID:000123456');
+  res.send('CPSY 350 Project: GitHub Actions CI. SAIT ID:000123456');
 });
 
-const server = app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
-
-module.exports = server;
+// Start server
+app.listen(PORT, HOST, () => {
+  console.log(`Server running at http://${HOST}:${PORT}`);
+});
